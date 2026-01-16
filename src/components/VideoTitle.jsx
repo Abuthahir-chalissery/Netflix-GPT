11eb2fa7f1e83bd20b54bc3bb9ff5d4283b9b8a4
Abuthahir-chalissery <abuthahirchalissery162@gmail.com>
2026-01-16 18:18:49 +0530

    Video Title added

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -4,11 +4,11 @@ const VideoTitle = ({title, overview}) => {
   return (
     <div className='flex flex-col gap-3'>
         <h1 className='text-4xl font-semibold text-gray-800'>{title}</h1>
-        <h1 className=' text-gray-600'>{overview}</h1>
+        <h1 className=' text-gray-600 w-1/3'>{overview}</h1>
 
-        <div className='flex gap-2'>
-            <button className='cursor-pointer bg-white text-black font-semibold p-1 w-20 rounded-sm border'>Play</button>
-            <button className='cursor-pointer bg-slate-400 text-white - p-1 w-25 rounded-sm '>More Info</button>
+        <div className='flex gap-3'>
+            <button className='cursor-pointer flex items-center justify-center gap-1 bg-white text-black font-semibold p-1 px-4 rounded-sm border'> <span><img className='w-4' src="/assets/play_icon.png" alt="" /></span> Play</button>
+            <button className='cursor-pointer flex items-center justify-center gap-1 bg-slate-400 text-white - p-1 px-2 rounded-sm '> <span><img className='w-6' src="/assets/info_icon.png" alt="" /></span> More Info</button>
         </div>
     </div>
   )
